e26c1339442633fa1fd25e5328fa4d8cf0062dd2
Maxim Dorofiyenko <maxdor@berkeley.edu>
2014-11-10 12:28:54 -0800

    better fix

--- hw6/bml_functions.R
+++ hw6/bml_functions.R
@@ -27,22 +27,24 @@ bml.init <- function(r, c, p){
 ## you can write extra functions that do just a step north or just a step east.
 
 move_east <- function(m) {
+  if (ncol(m) == 1) {
+    return(m)
+  }
   blocked <- m[, c(2:ncol(m), 1)]!= 0
   red_cars <- m * (m == 1)
   m * (m != 1) + red_cars * blocked + (red_cars * !blocked)[, c(ncol(m), 1:(ncol(m)-1))]
 }
 
 move_north <- function(m) {
+  if (nrow(m) == 1) {
+    return(m)
+  }
   blocked <- m[c(nrow(m), 1:(nrow(m)-1)), ]!= 0
   blue_cars <- m*(m == 2)
   m * (m != 2) + blue_cars * blocked + (blue_cars * !blocked)[c(2:nrow(m), 1), ]
 }
 
 bml.step <- function(m){
-  if (nrow(m) == 1 | length(m) == 1) {
-    return(list(m, FALSE))
-  }
-  
   m.new <- move_north(move_east(m))
   grid.new <- !identical(m.new, m)
   return(list(m.new, grid.new))
